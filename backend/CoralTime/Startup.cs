5673ed4b1f3bb9a3f7638782f97c20767cc144c6
CoralTeam <34846530+CoralTime@users.noreply.github.com>
2018-01-10 02:40:57 +0300

    Update Startup.cs
    A possibility of input of simple passwords.

--- backend/CoralTime/Startup.cs
+++ backend/CoralTime/Startup.cs
@@ -191,11 +191,11 @@ namespace CoralTime
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                options.Password.RequireDigit = true;
+                options.Password.RequireDigit = false;
                 options.Password.RequiredLength = 8;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
 
                 options.User.RequireUniqueEmail = true;
             });
