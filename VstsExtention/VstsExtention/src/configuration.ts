c5934990242164f6a67e99fc7583524ea8c1d823
Zeratulis <churpita.roman@gmail.com>
2018-10-15 19:07:40 +0300

    work with plugin

---
+++ VstsExtention/VstsExtention/src/configuration.ts
@@ -0,0 +1,99 @@
+// tslint:disable-next-line
+/// <reference path="isettings.d.ts" />
+// tslint:disable-next-line
+/// <reference path="iprojectContext.ts"/>
+
+import { IPromise } from "q";
+import Q = require("q");
+import Combos = require("VSS/Controls/Combos");
+import WebApi_Constants = require("VSS/WebApi/Constants");
+import "../static/_scss/styles.scss";
+import { IProjectContext } from "./iprojectContext";
+
+// import { settings } from "./telemetryClientSettings";
+
+// import TelemetryClient = require("scripts/TelemetryClient");
+
+export class Configuration {
+
+    private extentionSettings: ISettings;
+    private $siteUrl = $("#siteUrl-input");
+    private $userName = $("#userName-input");
+    private accessToken: string;
+    constructor() {
+        this.load();
+    }
+
+    public load() {
+        this.getKeyValueFromStorage("settings")
+            .then((savedSettings) => {
+                this.extentionSettings = (JSON.parse(savedSettings).data);
+                this.$siteUrl.val(this.extentionSettings.siteUrl);
+                this.$userName.val(this.extentionSettings.userName);
+            });
+        this.loadAccessToken();
+    }
+
+    public onSave() {
+        const settings = this.getCustomSettings();
+        this.setKeyValueInStorage("settings", settings);
+    }
+
+    public getExtensionContext(): IProjectContext {
+        const webContext = VSS.getWebContext();
+        const extensionContext: IProjectContext = {
+            projectId: webContext.project.id,
+            projectName: webContext.project.name,
+            userEmail: webContext.user.email,
+            userName: webContext.user.uniqueName,
+        };
+        return extensionContext;
+    }
+
+    public getSettings(): ISettings {
+        return this.extentionSettings;
+    }
+
+    public getAccessToken(): string {
+        return this.accessToken;
+    }
+
+    private getCustomSettings() {
+        const siteUrl = (this.$siteUrl.val() as string);
+        const userName = (this.$userName.val() as string);
+
+        const result = {
+            data: JSON.stringify({
+                siteUrl,
+                userName,
+            } as ISettings),
+        };
+        return result;
+    }
+
+    private setKeyValueInStorage(key, value): void {
+        VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: IExtensionDataService) => {
+            // Set value in user scope
+            dataService.setValue(key, value, { scopeType: "User" }).then((resvalue) => {
+                alert(resvalue);
+            });
+        });
+    }
+
+    private getKeyValueFromStorage(key): IPromise<string> {
+        const deferred = Q.defer<string>();
+        VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: IExtensionDataService) => {
+            // Get value from user scope
+            dataService.getValue(key, { scopeType: "User" }).then((value) => {
+                deferred.resolve(value as string);
+            });
+        });
+        return deferred.promise;
+    }
+
+    private loadAccessToken() {
+        VSS.getAppToken().then((token) => {
+            this.accessToken = token.token;
+        });
+    }
+}
