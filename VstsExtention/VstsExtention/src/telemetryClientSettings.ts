b9c2d644bbc3fa09736f6dbe8dfd512f7788e77a
Denis <stive-11@mail.ru>
2018-10-16 17:01:51 +0300

    Cleanup VSTS project

--- VstsExtention/VstsExtention/src/telemetryClientSettings.ts
+++
@@ -1,9 +0,0 @@
-import * as tc from "telemetryclient-team-services-extension";
-
-export const settings: tc.TelemetryClientSettings = {
-    disableAjaxTracking: "true",
-    disableTelemetry: "true",
-    enableDebug: "false",
-    extensioncontext: "CoralTimeTracker",
-    key: "__InstrumentationKey__",
-};
