770476c3508216dd4b8489e3071e46f2ba93dec1
Denis <stive-11@mail.ru>
2018-12-19 12:47:44 +0300

    Fix IIS hosting

--- backend/CoralTime/Program.cs
+++ backend/CoralTime/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CoralTime.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
@@ -42,8 +43,8 @@ namespace CoralTime
         public static IWebHostBuilder CreateDefaultBuilder(string[] args)
         {
             var builder = new WebHostBuilder()
-                .UseApplicationInsights()                
-                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseApplicationInsights()
+                .UseContentRoot(Environment.CurrentDirectory)
                 .UseKestrel()
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
